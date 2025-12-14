a32c087295a9ecaab66a5528603d1cfdd03e1504
Nitesh <niteshanthony3@gmail.com>
2025-12-13 22:58:19 -0500

    installed the setup and assets

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+const App = () => {
+  return (
+    <main>
+      <div className="">
+        <h1 className="">Welcome To my Portfolio</h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
