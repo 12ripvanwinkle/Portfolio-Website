309505b54891dd72422f53819eedb6e45f70f165
Nitesh <niteshanthony3@gmail.com>
2026-01-21 15:25:45 -0500

    implemented the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Navbar from './components/Navbar'
 import Hero from './components/sections/Hero'
 import ShowcaseSection from './components/sections/ShowcaseSection'
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Hero />
       <ShowcaseSection />
     </>
